54c6cccb38ca39fcb1b007e2fbb814a107be2357
khaled <khaledleader012@gmail.com>
2025-08-19 13:46:25 +0300

    add sign up method v1

--- registry-service/src/main/java/com/example/registry_service/service/UserService.java
+++ registry-service/src/main/java/com/example/registry_service/service/UserService.java
@@ -1,4 +1,8 @@
 package com.example.registry_service.service;
 
+import com.example.registry_service.dto.UserDTO;
+
 public interface UserService {
+    UserDTO login(UserDTO userDTO);
+    UserDTO signup(UserDTO userDTO);
 }
--- registry-service/src/main/java/com/example/registry_service/service/impl/UserServiceImpl.java
+++ registry-service/src/main/java/com/example/registry_service/service/impl/UserServiceImpl.java
@@ -1,9 +1,34 @@
 package com.example.registry_service.service.impl;
 
 
+import com.example.registry_service.controller.UserController;
+import com.example.registry_service.converter.UserConverter;
+import com.example.registry_service.dto.UserDTO;
+import com.example.registry_service.entity.UserEntity;
+import com.example.registry_service.repository.UserRepository;
 import com.example.registry_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserConverter userConverter;
+
+    @Override
+    public UserDTO login(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public UserDTO signup(UserDTO userDTO) {
+        UserEntity user = userConverter.convertUserDTOToEntity(userDTO);
+        user = userRepository.save(user);
+        userDTO = userConverter.convertUserEntityToDTO(user);
+        return userDTO;
+    }
 }
